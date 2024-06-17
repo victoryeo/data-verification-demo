6727d39d68685348781cc6dad9fdfccd1c694c4f
yeosiowvic <victoryeo1974@gmail.com>
2024-06-17 13:18:25 +0800

    update hasura redirection

--- pages/api/hasura-integration/[[...path]].ts
+++ pages/api/hasura-integration/[[...path]].ts
@@ -1,32 +1,29 @@
-import { getCookies } from "cookies-next";
-import { createProxyMiddleware } from "http-proxy-middleware";
+import type { NextApiRequest, NextApiResponse } from "next";
+import httpProxyMiddleware from "next-http-proxy-middleware";
 
-export default createProxyMiddleware({
-  target: process.env.HASURA_INTEGRATION_HOST,
-  changeOrigin: true,
-  pathRewrite: {
-    "^/api/hasura-integration": `/v1/graphql`,
-  },
-  xfwd: true,
-  headers: {
-    "x-hasura-admin-secret": `${process.env.HASURA_ADMIN_SECRET}`,
-    "x-auth-token":`${process.env.NEXT_PUBLIC_API_KEY}`
-  },
-  onProxyReq: (proxyReq, req) => {
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    const cookies = getCookies({ req });
-    proxyReq.setHeader(
-      "x-hasura-user-id",
-      cookies["x-hasura-user-id"] ?? "anonymous"
-    );
-    proxyReq.setHeader("x-hasura-role", cookies["x-hasura-role"] ?? "admin");
-  },
-});
+const hasuraHandler = (req: NextApiRequest, res: NextApiResponse) => (
+  httpProxyMiddleware(req, res, {
+    // You can use the `http-proxy` option
+    target: process.env.HASURA_INTEGRATION_HOST,
+    // In addition, you can use the `pathRewrite` option provided by `next-http-proxy-middleware`
+    pathRewrite: [{
+      patternStr: "^/api/hasura-integration",
+      replaceStr: "/v1/graphql"
+    }],
+    headers: {
+      'x-hasura-admin-secret': `${process.env.HASURA_ADMIN_SECRET}`,
+      'x-auth-token': `${process.env.NEXT_PUBLIC_API_KEY}`
+    },
+    changeOrigin: true,
+    xfwd: true,
+  })
+);
 
 export const config = {
   api: {
     bodyParser: false, // enable POST requests
-    externalResolver: false, // hide warning message
+    externalResolver: true, // hide warning message
   },
 };
+
+export default hasuraHandler;
