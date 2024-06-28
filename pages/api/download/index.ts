888af6d4bf9e4b2112fea3fce8d927c154531255
yeosiowvic <victoryeo1974@gmail.com>
2024-06-28 11:18:34 +0800

    change url

--- pages/api/download/index.ts
+++ pages/api/download/index.ts
@@ -1,37 +1,8 @@
-import {
-  BlobServiceClient,
-  ContainerClient,
-  BlockBlobClient,
-  generateBlobSASQueryParameters,
-  BlobSASPermissions,
-} from '@azure/storage-blob';
 import { NextApiRequest, NextApiResponse } from 'next';
 
 async function getBlobUrl(blobName: string) {
-  console.log('AZ_SEC', process.env.NEXT_PUBLIC_AZ_SECRET)
-  const connectionString =
-    "DefaultEndpointsProtocol=https;AccountName=" + process.env.AZ_NAME + ";AccountKey=" + process.env.NEXT_PUBLIC_AZ_SECRET + ";EndpointSuffix=core.windows.net";
-  const containerName = 'containerkhi';
-
-  const blobServiceClient =
-    BlobServiceClient.fromConnectionString(connectionString);
-  const containerClient = blobServiceClient.getContainerClient(containerName);
-
-  const blockBlobClient = containerClient.getBlockBlobClient(blobName);
-
-  const sasQueryParams = generateBlobSASQueryParameters(
-    {
-      containerName,
-      blobName,
-      permissions: BlobSASPermissions.parse('r'),
-      startsOn: new Date(),
-      expiresOn: new Date(new Date().valueOf() + 3600 * 1000),
-    },
-    //@ts-ignore
-    blockBlobClient.credential
-  );
-
-  const sasUrl = `${blockBlobClient.url}?${sasQueryParams.toString()}`;
+  // url
+  const sasUrl = 'https://'+`${process.env.MINIO_ENDPOINT}` + '/data/' + blobName;
 
   return sasUrl;
 }
@@ -43,7 +14,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 
   try {
     const sasUrl = await getBlobUrl(body.blobName);
-
+    console.log('sasUrl', sasUrl)
     // Send the SAS URL as a JSON response
     res.status(200).json({ downloadUrl: sasUrl });
   } catch (error) {
